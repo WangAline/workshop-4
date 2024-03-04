d46a53d5245826cb0abf4daef97c6860936be8b1
WangAline <aline.Wang@edu.devinci.fr>
2024-03-04 10:25:29 +0100

    ok 3.

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+import { BASE_ONION_ROUTER_PORT} from "../config";
 import * as crypto from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
@@ -12,15 +12,11 @@ export async function simpleOnionRouter(nodeId: number) {
   let lastReceivedEncryptedMessage: string | null = null;
   let lastReceivedDecryptedMessage: string | null = null;
   let lastMessageDestination: number | null = null;
+  
   const { publicKey, privateKey } = await crypto.generateRsaKeyPair();
   let PriK=await crypto.exportPrvKey(privateKey)
-  let PubK = await crypto.exportPubKey(publicKey);
-
-  const data = JSON.stringify({
-    nodeId,
-    pubKey: PubK,
-  });
-
+  let PubK=await crypto.exportPubKey(publicKey)
+  
   // Route for the last received encrypted message
   onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
     res.json({ result: lastReceivedEncryptedMessage });
@@ -41,10 +37,19 @@ export async function simpleOnionRouter(nodeId: number) {
   });
 
   onionRouter.get('/getPrivateKey', (req, res) => {
-    const base64PrivateKey = Buffer.from(PriK);
-    res.json({ result: base64PrivateKey });
+    res.json({ result: PriK }); 
   });
   
+  await fetch(`http://localhost:8080/registerNode`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({
+      nodeId: nodeId,
+      pubKey: PubK,
+    })
+  });
+
+
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
       `Onion router ${nodeId} is listening on port ${
