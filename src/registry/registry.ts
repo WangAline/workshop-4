5fc01d68e81b294f42e3358726ef180833f4f77d
WangAline <aline.Wang@edu.devinci.fr>
2024-03-04 08:19:40 +0100

    update 3.1

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -23,7 +23,27 @@ export async function launchRegistry() {
   _registry.get("/status", (req: Request, res: Response) => {
     res.send("live");
   });
+
   
+  let nodeRegistry: Node[] = [];
+
+  _registry.post('/registerNode', (req: Request, res: Response) => {
+    const { nodeId, pubKey } = req.body;
+
+    const newNode: Node = {
+      nodeId: nodeId,
+      pubKey: pubKey,
+    };
+
+    nodeRegistry.push(newNode);
+
+    return res.status(200).send({ message: "Node registered successfully." });
+  });
+
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json({ nodes: nodeRegistry });
+  });
+
   
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
